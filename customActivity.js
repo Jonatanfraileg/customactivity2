0bd3306a5e4486bc9236fffbd08d858c19a23320
Jonatanfraileg <36997743+Jonatanfraileg@users.noreply.github.com>
2018-06-27 11:13:10 +0200

    Add files via upload

---
+++ customActivity.js
@@ -0,0 +1,158 @@
+define([
+    'js/postmonger'
+], function(
+    Postmonger
+) {
+    'use strict';
+
+    var connection = new Postmonger.Session();
+    var payload = {};
+    var steps = [ // initialize to the same value as what's set in config.json for consistency
+        { "label": "Step 1", "key": "step1" },
+        { "label": "Step 2", "key": "step2" }
+    ];
+    var currentStep = steps[0].key;
+
+    $(window).ready(onRender);
+
+    connection.on('initActivity', initialize);
+    connection.on('requestedTokens', onGetTokens);
+    connection.on('requestedEndpoints', onGetEndpoints);
+
+    connection.on('clickedNext', onClickedNext);
+    connection.on('clickedBack', onClickedBack);
+    connection.on('gotoStep', onGotoStep);
+
+    function onRender() {
+        // JB will respond the first time 'ready' is called with 'initActivity'
+        connection.trigger('ready');
+
+        connection.trigger('requestTokens');
+        connection.trigger('requestEndpoints');
+        connection.trigger('updateButton', { button: 'next', enabled: 'true' });
+       
+    }
+
+    function initialize (data) {
+        if (data) {
+            payload = data;
+        }
+
+        var message;
+        var hasInArguments = Boolean(
+            payload['arguments'] &&
+            payload['arguments'].execute &&
+            payload['arguments'].execute.inArguments &&
+            payload['arguments'].execute.inArguments.length > 0
+        );
+
+        var inArguments = hasInArguments ? payload['arguments'].execute.inArguments : {};
+
+        $.each(inArguments, function(index, inArgument) {
+            $.each(inArgument, function(key, val) {
+                if (key === 'message') {
+                    message = val;
+                }
+            });
+        });
+
+        // If there is no message selected, disable the next button
+        if (!message) {
+            showStep(null, 1);
+            // If there is a message, skip to the summary step
+        } else {
+            $('#message').html(message);
+            connection.trigger('nextStep');
+            showStep(null, 2);
+        }
+    }
+
+    function onGetTokens (tokens) {
+        // Response: tokens = { token: <legacy token>, fuel2token: <fuel api token> }
+        // console.log(tokens);
+    }
+
+    function onGetEndpoints (endpoints) {
+        // Response: endpoints = { restHost: <url> } i.e. "rest.s1.qa1.exacttarget.com"
+        // console.log(endpoints);
+    }
+
+    function onClickedNext () {
+        if (
+            (currentStep.key === 'step2') 
+        ) {
+            save();
+        } else {
+            var message = getMessage();
+            $('#message').html(message);
+            connection.trigger('nextStep');
+        }
+    }
+
+    function onClickedBack () {
+        connection.trigger('prevStep');
+    }
+
+    function onGotoStep (step) {
+        showStep(step);
+        connection.trigger('ready');
+    }
+
+    function showStep(step, stepIndex) {
+        if (stepIndex && !step) {
+            step = steps[stepIndex-1];
+        }
+
+        currentStep = step;
+
+        $('.step').hide();
+
+        switch(currentStep.key) {
+            case 'step1':
+                $('#step1').show();
+                connection.trigger('updateButton', {
+                    button: 'next',
+                    enabled: Boolean(getMessage())
+                });
+                connection.trigger('updateButton', {
+                    button: 'back',
+                    visible: false
+                });
+                break;
+            case 'step2':
+                $('#step2').show();
+                connection.trigger('updateButton', {
+                    button: 'back',
+                    visible: true
+                });
+                connection.trigger('updateButton', {
+                    button: 'next',
+                    text: 'done',
+                    visible: true
+                });
+                break;
+        }
+    }
+
+    function save() {
+        
+        var value = getMessage();
+        console.log('ciao='+ value);
+        // 'payload' is initialized on 'initActivity' above.
+        // Journey Builder sends an initial payload with defaults
+        // set by this activity's config.json file.  Any property
+        // may be overridden as desired.
+
+        payload['arguments'].execute.inArguments = [{ "message": value }];
+
+        payload['metaData'].isConfigured = true;
+
+        connection.trigger('updateActivity', payload);
+    }
+
+    function getMessage() {
+        return  $('#select1').val();
+
+    }
+
+});
